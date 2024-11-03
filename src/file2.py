913b734d1091b9a7df7743ddea879b9d0208dcc8
IslamEdres <eng.islam.voip@gmail.com>
2024-11-03 15:11:34 +0200

    add a nsw file from local

--- src/file2.py
+++ src/file2.py
@@ -1,3 +1,8 @@
 This is the seecnd file here on github 
 and you can check your local log git log you will find differnt commit and you must use git pull to get the lastest version after edit you eill 
 add and commet on the same line git commit -a -m "add new file"
+
+
+
+
+
